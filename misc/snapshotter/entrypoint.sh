a13beb674c071f5b97676acff63a0fc96dc8db68
zyfjeff <tianqian.zyf@alibaba-inc.com>
2022-05-25 18:13:52 +0800

    Add localfs test
    Signed-off-by: zyfjeff <tianqian.zyf@alibaba-inc.com>

--- misc/snapshotter/entrypoint.sh
+++ misc/snapshotter/entrypoint.sh
@@ -3,12 +3,15 @@
 CONTAINERD_ROOT="${CONTAINERD_ROOT:-/var/lib/containerd/}"
 
 set -eu
-
+MODE="config"
 if [ "$#" -eq 0 ]; then
+	if [ ! -z ${BACKEND_TYPE} == "localfs" ];then 
+		MODE=${BACKEND_TYPE}
+	fi
 	containerd-nydus-grpc \
 	    --log-level trace \
 	    --nydusd-path /usr/local/bin/nydusd \
-	    --config-path /etc/nydus/config.json \
+	    --config-path /etc/nydus/${MODE}.json \
 	    --root ${CONTAINERD_ROOT}/io.containerd.snapshotter.v1.nydus \
 	    --address ${CONTAINERD_ROOT}/io.containerd.snapshotter.v1.nydus/containerd-nydus-grpc.sock \
 	    --enable-nydus-overlayfs \
