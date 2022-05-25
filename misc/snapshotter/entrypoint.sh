90883686110a692204e6939fe6a5a54d282d1ee2
zyfjeff <tianqian.zyf@alibaba-inc.com>
2022-05-25 18:23:38 +0800

    Fix backend_type env
    Signed-off-by: zyfjeff <tianqian.zyf@alibaba-inc.com>

--- misc/snapshotter/entrypoint.sh
+++ misc/snapshotter/entrypoint.sh
@@ -3,15 +3,12 @@
 CONTAINERD_ROOT="${CONTAINERD_ROOT:-/var/lib/containerd/}"
 
 set -eu
-MODE="config"
+BACKEND_TYPE="${BACKEND_TYPE:-config}"
 if [ "$#" -eq 0 ]; then
-	if [ ! -z ${BACKEND_TYPE} ];then 
-		MODE=${BACKEND_TYPE}
-	fi
 	containerd-nydus-grpc \
 	    --log-level trace \
 	    --nydusd-path /usr/local/bin/nydusd \
-	    --config-path /etc/nydus/${MODE}.json \
+	    --config-path /etc/nydus/${BACKEND_TYPE}.json \
 	    --root ${CONTAINERD_ROOT}/io.containerd.snapshotter.v1.nydus \
 	    --address ${CONTAINERD_ROOT}/io.containerd.snapshotter.v1.nydus/containerd-nydus-grpc.sock \
 	    --enable-nydus-overlayfs \
