ef26ac4d97bc818c73f20bd4b9efdbea31faf8ed
loheagn <loheagn@icloud.com>
2022-06-17 09:26:39 +0000

    export `label.AppendLabelsHandlerWrapper` function.
    Nerdctl can append image's basic informations to each layer descriptor during unpack by importing this function.
    
    Signed-off-by: loheagn <loheagn@icloud.com>

--- pkg/label/label.go
+++ pkg/label/label.go
@@ -9,14 +9,17 @@ package label
 const (
 	Signature = "containerd.io/snapshot/nydus-signature"
 
-	ImageRef          = "containerd.io/snapshot/cri.image-ref"
+	CRIImageRef    = "containerd.io/snapshot/cri.image-ref"
+	CRIImageLayers = "containerd.io/snapshot/cri.image-layers"
+	CRILayerDigest = "containerd.io/snapshot/cri.layer-digest"
+
 	ImagePullSecret   = "containerd.io/snapshot/pullsecret"
 	ImagePullUsername = "containerd.io/snapshot/pullusername"
 
-	TargetSnapshotLabel = "containerd.io/snapshot.ref"
-	CRIImageLayer       = "containerd.io/snapshot/cri.image-layers"
-	CRIDigest           = "containerd.io/snapshot/cri.layer-digest"
-	RemoteLabel         = "containerd.io/snapshot/remote"
-	NydusMetaLayer      = "containerd.io/snapshot/nydus-bootstrap"
-	NydusDataLayer      = "containerd.io/snapshot/nydus-blob"
+	TargetSnapshotLabel       = "containerd.io/snapshot.ref"
+	TargetManifestDigestLabel = "containerd.io/snapshot/cri.manifest-digest"
+	RemoteLabel               = "containerd.io/snapshot/remote"
+
+	NydusMetaLayer = "containerd.io/snapshot/nydus-bootstrap"
+	NydusDataLayer = "containerd.io/snapshot/nydus-blob"
 )
