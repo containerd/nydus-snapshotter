14ea45c9ad684e922cbc26a9e8d6c8f7c6930314
Changwei Ge <gechangwei@bytedance.com>
2022-08-15 19:40:46 +0800

    github: perform test against multiple daemon mode
    Now only shared daemon mode is performed. Also do it
    for multipe daemon mode.
    
    Signed-off-by: Changwei Ge <gechangwei@bytedance.com>

--- misc/snapshotter/entrypoint.sh
+++ misc/snapshotter/entrypoint.sh
@@ -4,17 +4,19 @@ CONTAINERD_ROOT="${CONTAINERD_ROOT:-/var/lib/containerd/}"
 
 set -eu
 BACKEND_TYPE="${BACKEND_TYPE:-config}"
+NYDUSD_DAEMON_MODE="${NYDUSD_DAEMON_MODE:-shared}"
+
 if [ "$#" -eq 0 ]; then
 	containerd-nydus-grpc \
-	    --log-level trace \
-	    --nydusd-path /usr/local/bin/nydusd \
-	    --config-path /etc/nydus/${BACKEND_TYPE}.json \
-	    --root ${CONTAINERD_ROOT}/io.containerd.snapshotter.v1.nydus \
-	    --address ${CONTAINERD_ROOT}/io.containerd.snapshotter.v1.nydus/containerd-nydus-grpc.sock \
-	    --enable-nydus-overlayfs \
-	    --daemon-mode shared \
-			--enable-stargz \
-	    --log-to-stdout
+		--log-level info \
+		--nydusd-path /usr/local/bin/nydusd \
+		--config-path /etc/nydus/${BACKEND_TYPE}.json \
+		--root ${CONTAINERD_ROOT}/io.containerd.snapshotter.v1.nydus \
+		--address ${CONTAINERD_ROOT}/io.containerd.snapshotter.v1.nydus/containerd-nydus-grpc.sock \
+		--enable-nydus-overlayfs \
+		--daemon-mode ${NYDUSD_DAEMON_MODE} \
+		--enable-stargz \
+		--log-to-stdout
 fi
 
 exec $@
