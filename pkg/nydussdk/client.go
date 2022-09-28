e3ecd74d1e8c848e57ef1e6a227b48e7b4ca3d16
Changwei Ge <gechangwei@bytedance.com>
2022-09-28 14:26:51 +0800

    refactor nydusd http clients package
    Signed-off-by: Changwei Ge <gechangwei@bytedance.com>

--- pkg/nydussdk/client.go
+++ pkg/nydussdk/client.go
@@ -11,16 +11,15 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/fs"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 
 	"github.com/pkg/errors"
 
-	"github.com/containerd/containerd/log"
 	"github.com/containerd/nydus-snapshotter/config"
 	"github.com/containerd/nydus-snapshotter/pkg/nydussdk/model"
 	"github.com/containerd/nydus-snapshotter/pkg/utils/retry"
@@ -30,9 +29,10 @@ const (
 	endpointDaemonInfo = "/api/v1/daemon"
 	endpointMount      = "/api/v1/mount"
 	endpointMetrics    = "/api/v1/metrics"
+	endpointBlobs      = "/api/v2/blobs"
 
 	defaultHTTPClientTimeout = 30 * time.Second
-	contentType              = "application/json"
+	jsonContentType          = "application/json"
 )
 
 type Interface interface {
@@ -48,156 +48,54 @@ type NydusdClient struct {
 	httpClient *http.Client
 }
 
-func NewNydusClient(sock string) (Interface, error) {
-	transport, err := buildTransport(sock)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to build transport for nydus client")
+type query = url.Values
+
+func (c *NydusdClient) url(path string, query query) (url string) {
+	url = fmt.Sprintf("http://unix%s", path)
+
+	if len(query) != 0 {
+		url += "?" + query.Encode()
 	}
-	return &NydusdClient{
-		httpClient: &http.Client{
-			Timeout:   defaultHTTPClientTimeout,
-			Transport: transport,
+
+	return
+}
+
+func succeeded(resp *http.Response) bool {
+	return resp.StatusCode == http.StatusNoContent || resp.StatusCode == http.StatusOK
+}
+
+func decode(resp *http.Response, v interface{}) error {
+	if err := json.NewDecoder(resp.Body).Decode(&v); err != nil {
+		return errors.Wrap(err, "decode response")
+	}
+
+	return nil
+}
+
+func parseErrorMessage(resp *http.Response) error {
+	var errMessage model.ErrorMessage
+	err := decode(resp, &errMessage)
+	if err != nil {
+		return err
+	}
+
+	return errors.Errorf("http response: %d, error code: %s, error message: %s",
+		resp.StatusCode, errMessage.Code, errMessage.Message)
+}
+
+func buildTransport(sock string) (http.RoundTripper, error) {
+	return &http.Transport{
+		MaxIdleConns:          10,
+		IdleConnTimeout:       10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
+			dialer := &net.Dialer{
+				Timeout:   5 * time.Second,
+				KeepAlive: 5 * time.Second,
+			}
+			return dialer.DialContext(ctx, "unix", sock)
 		},
 	}, nil
-}
-
-func (c *NydusdClient) CheckStatus() (*model.DaemonInfo, error) {
-	addr := fmt.Sprintf("http://unix%s", endpointDaemonInfo)
-	resp, err := c.httpClient.Get(addr)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to do HTTP GET from %s", addr)
-	}
-	defer resp.Body.Close()
-	b, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to read status response")
-	}
-	var info model.DaemonInfo
-	if err = json.Unmarshal(b, &info); err != nil {
-		return nil, err
-	}
-	return &info, nil
-}
-
-func (c *NydusdClient) Umount(sharedMountPoint string) error {
-	requestURL := fmt.Sprintf("http://unix%s?mountpoint=%s", endpointMount, sharedMountPoint)
-	req, err := http.NewRequest(http.MethodDelete, requestURL, nil)
-	if err != nil {
-		return errors.Wrap(err, "failed to create umount request")
-	}
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return errors.Wrapf(err, "failed to do HTTP DELETE to %s", requestURL)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusNoContent {
-		return nil
-	}
-	return handleMountError(resp)
-}
-
-func (c *NydusdClient) GetFsMetric(sharedDaemon bool, sid string) (*model.FsMetric, error) {
-	var getStatURL string
-
-	if sharedDaemon {
-		getStatURL = fmt.Sprintf("http://unix%s?id=/%s/fs", endpointMetrics, sid)
-	} else {
-		getStatURL = fmt.Sprintf("http://unix%s", endpointMetrics)
-	}
-
-	req, err := http.NewRequest(http.MethodGet, getStatURL, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create GetFsMetric request")
-	}
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to do HTTP GET to %s", getStatURL)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusNoContent {
-		return nil, errors.New("got unexpected http status StatusNoContent")
-	}
-
-	var m model.FsMetric
-	if err = json.NewDecoder(resp.Body).Decode(&m); err != nil {
-		return nil, errors.Wrap(err, "failed to decode FsMetric")
-	}
-	return &m, nil
-}
-
-func (c *NydusdClient) SharedMount(sharedMountPoint, bootstrap, daemonConfig string) error {
-	requestURL := fmt.Sprintf("http://unix%s?mountpoint=%s", endpointMount, sharedMountPoint)
-	content, err := os.ReadFile(daemonConfig)
-	if err != nil {
-		return errors.Wrapf(err, "failed to get content of daemon config %s", daemonConfig)
-	}
-	body, err := json.Marshal(model.NewMountRequest(bootstrap, string(content)))
-	if err != nil {
-		return errors.Wrap(err, "failed to create mount request")
-	}
-	resp, err := c.httpClient.Post(requestURL, contentType, bytes.NewBuffer(body))
-	if err != nil {
-		return errors.Wrapf(err, "failed to do HTTP POST to %s", requestURL)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusNoContent {
-		return nil
-	}
-	return handleMountError(resp)
-}
-
-func (c NydusdClient) FscacheBindBlob(daemonConfig string) error {
-	log.L.Infof("requesting daemon to bind fscache blob with config %s", daemonConfig)
-
-	body, err := os.ReadFile(daemonConfig)
-	if err != nil {
-		return errors.Wrapf(err, "failed to get content of daemon config %s", daemonConfig)
-	}
-
-	requestURL := "http://unix/api/v2/blobs"
-	req, err := http.NewRequest(http.MethodPut, requestURL, bytes.NewBuffer(body))
-	if err != nil {
-		return errors.Wrapf(err, "failed to create request for url %s", requestURL)
-	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return errors.Wrapf(err, "failed to do HTTP PUT to %s", requestURL)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusNoContent {
-		return nil
-	}
-
-	return handleMountError(resp)
-}
-
-func (c NydusdClient) FscacheUnbindBlob(daemonConfig string) error {
-	body, err := os.ReadFile(daemonConfig)
-	if err != nil {
-		return errors.Wrapf(err, "failed to get content of daemon config %s", daemonConfig)
-	}
-
-	var cfg config.DaemonConfig
-	if err := json.Unmarshal(body, &cfg); err != nil {
-		return errors.Wrap(err, "unmarshal fscache daemon config")
-	}
-
-	requestURL := fmt.Sprintf("http://unix/api/v2/blobs?domain_id=%s", cfg.DomainID)
-	req, err := http.NewRequest(http.MethodDelete, requestURL, nil)
-	if err != nil {
-		return errors.Wrap(err, "failed to create fscache unbind blob request")
-	}
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return errors.Wrapf(err, "failed to do HTTP DELETE to %s", requestURL)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusNoContent {
-		return nil
-	}
-	return handleMountError(resp)
 }
 
 func WaitUntilSocketExisted(sock string) error {
@@ -218,35 +116,148 @@ func WaitUntilSocketExisted(sock string) error {
 		retry.Delay(100*time.Millisecond))
 }
 
-// TODO: It's time-consuming. Make it execute only once by sync.Once
-func buildTransport(sock string) (http.RoundTripper, error) {
+func NewNydusClient(sock string) (Interface, error) {
 	err := WaitUntilSocketExisted(sock)
 	if err != nil {
 		return nil, err
 	}
-	return &http.Transport{
-		MaxIdleConns:          10,
-		IdleConnTimeout:       10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-		DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
-			dialer := &net.Dialer{
-				Timeout:   5 * time.Second,
-				KeepAlive: 5 * time.Second,
-			}
-			return dialer.DialContext(ctx, "unix", sock)
+	transport, err := buildTransport(sock)
+	if err != nil {
+		return nil, errors.Wrap(err, "build nydusd http client")
+	}
+	return &NydusdClient{
+		httpClient: &http.Client{
+			Timeout:   defaultHTTPClientTimeout,
+			Transport: transport,
 		},
 	}, nil
 }
 
-func handleMountError(resp *http.Response) error {
-	var r io.Reader = resp.Body
-	b, err := io.ReadAll(r)
+func (c *NydusdClient) CheckStatus() (*model.DaemonInfo, error) {
+	url := c.url(endpointDaemonInfo, query{})
+	resp, err := c.httpClient.Get(url)
 	if err != nil {
-		return errors.Wrap(err, "failed to read from reader")
+		return nil, errors.Wrapf(err, "get daemon info %s", url)
 	}
-	var errMessage model.ErrorMessage
-	if err = json.Unmarshal(b, &errMessage); err != nil {
-		return errors.Wrap(err, "failed unmarshal ErrorMessage")
+	defer resp.Body.Close()
+
+	if succeeded(resp) {
+		var info model.DaemonInfo
+		if err = decode(resp, &info); err != nil {
+			return nil, err
+		}
+		return &info, nil
 	}
-	return fmt.Errorf("http response: %d, error code: %s, error message: %s", resp.StatusCode, errMessage.Code, errMessage.Message)
+
+	return nil, parseErrorMessage(resp)
+}
+
+func (c *NydusdClient) Umount(mp string) error {
+	query := query{}
+	query.Add("mountpoint", mp)
+	url := c.url(endpointMount, query)
+	return c.simpleRequest(http.MethodDelete, url)
+}
+
+func (c *NydusdClient) GetFsMetric(sharedDaemon bool, sid string) (*model.FsMetric, error) {
+	query := query{}
+	if sharedDaemon {
+		query.Add("id", "/"+sid)
+	}
+
+	url := c.url(endpointMetrics, query)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, errors.Wrapf(err, "construct request url %s", url)
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if !succeeded(resp) {
+		return nil, parseErrorMessage(resp)
+	}
+
+	var m model.FsMetric
+	err = decode(resp, &m)
+	if err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
+
+func (c *NydusdClient) SharedMount(mp, bootstrap, daemonConfig string) error {
+	config, err := os.ReadFile(daemonConfig)
+	if err != nil {
+		return errors.Wrapf(err, "read nydusd configurations %s", daemonConfig)
+	}
+
+	body, err := json.Marshal(model.NewMountRequest(bootstrap, string(config)))
+	if err != nil {
+		return errors.Wrap(err, "construct mount request")
+	}
+
+	query := query{}
+	query.Add("mountpoint", mp)
+	url := c.url(endpointMount, query)
+
+	resp, err := c.httpClient.Post(url, jsonContentType, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if succeeded(resp) {
+		return nil
+	}
+
+	return parseErrorMessage(resp)
+}
+
+func (c *NydusdClient) FscacheBindBlob(daemonConfig string) error {
+	// FIXME: it brings extra IO latency!
+	body, err := os.ReadFile(daemonConfig)
+	if err != nil {
+		return errors.Wrapf(err, "read daemon configuration %s", daemonConfig)
+	}
+
+	url := c.url(endpointBlobs, query{})
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(body))
+	if err != nil {
+		return errors.Wrapf(err, "construct request, url %s", url)
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if succeeded(resp) {
+		return nil
+	}
+
+	return parseErrorMessage(resp)
+}
+
+func (c *NydusdClient) FscacheUnbindBlob(daemonConfig string) error {
+	f, err := os.ReadFile(daemonConfig)
+	if err != nil {
+		return errors.Wrapf(err, "read daemon configuration %s", daemonConfig)
+	}
+
+	var cfg config.DaemonConfig
+	if err := json.Unmarshal(f, &cfg); err != nil {
+		return errors.Wrap(err, "unmarshal daemon configuration")
+	}
+
+	query := query{}
+	query.Add("domain_id", cfg.DomainID)
+	url := c.url(endpointBlobs, query)
+
+	return c.simpleRequest(http.MethodDelete, url)
 }
