3df98a55d7e47ecffa32245163521c46fc38bf93
Changwei Ge <gechangwei@bytedance.com>
2022-10-26 10:15:14 +0800

    use basic log infra to log rather than directly using logrus
    Signed-off-by: Changwei Ge <gechangwei@bytedance.com>

--- pkg/utils/erofs/erofs.go
+++ pkg/utils/erofs/erofs.go
@@ -6,9 +6,9 @@ package erofs
 import (
 	"fmt"
 
+	"github.com/containerd/containerd/log"
 	"github.com/opencontainers/go-digest"
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 )
 
@@ -21,11 +21,11 @@ func Mount(bootstrapPath, domainID, fscacheID, mountPoint string) error {
 	} else {
 		opts = "fsid=" + fscacheID
 	}
-	logrus.Infof("Mount erofs to %s with options %s", mountPoint, opts)
+	log.L.Infof("Mount erofs to %s with options %s", mountPoint, opts)
 
 	if err := mount("erofs", mountPoint, "erofs", 0, opts); err != nil {
 		if errors.Is(err, unix.EINVAL) && domainID != "" {
-			logrus.Errorf("mount erofs with shared domain failed," +
+			log.L.Errorf("mount erofs with shared domain failed," +
 				"If using this feature, make sure your Linux kernel version >= 6.1")
 		}
 		return errors.Wrapf(err, "failed to mount erofs")
