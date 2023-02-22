335b16922fc09c04afcb8291d94f72cdc3192906
Bin Tang <tangbin.bin@bytedance.com>
2023-02-22 17:12:33 +0800

    modify new http listener function to be distincted with pprof
    Signed-off-by: Bin Tang <tangbin.bin@bytedance.com>

--- snapshot/snapshot.go
+++ snapshot/snapshot.go
@@ -113,7 +113,7 @@ func NewSnapshotter(ctx context.Context, cfg *config.SnapshotterConfig) (snapsho
 	// Start to collect metrics.
 	if cfg.MetricsConfig.Address != "" {
 		go func() {
-			if err := metrics.NewHTTPListener(cfg.MetricsConfig.Address); err != nil {
+			if err := metrics.NewMetricsHTTPListener(cfg.MetricsConfig.Address); err != nil {
 				log.L.WithError(err).Error("Failed to start metrics HTTP server")
 			}
 		}()
