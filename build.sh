40fedf0506120740e31016e23b23867f5864e871
Bin Tang <tangbin.bin@bytedance.com>
2023-06-16 17:38:23 +0800

    feat(build.sh): install rust environment for building optimizer server
    Signed-off-by: Bin Tang <tangbin.bin@bytedance.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+export RUSTUP_DIST_SERVER="https://rsproxy.cn"
+export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
+
+# install rust environment
+curl --proto '=https' --tlsv1.2 -sSf https://rsproxy.cn/rustup-init.sh | sh -s -- -y
+source "$HOME/.cargo/env"
+
 make static-release
 
 mkdir -p output
