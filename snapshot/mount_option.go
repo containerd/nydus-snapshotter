3b89f95a24a89ba05791c1b9cc95b2dbd25af51a
Jiang Liu <gerry@linux.alibaba.com>
2023-09-05 16:36:34 +0800

    snapshot: introduce data structures to support Kata virtual volumes
    Introduce data structures to support Kata virtual volumes, which is a
    superset of Nydus `ExtraOption`.
    
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>
    Signed-off-by: Jiang Liu <gerry@linux.alibaba.com>

--- snapshot/mount_option.go
+++ snapshot/mount_option.go
@@ -9,9 +9,11 @@ package snapshot
 import (
 	"context"
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/containerd/containerd/log"
 	"github.com/containerd/containerd/mount"
@@ -96,4 +98,179 @@ func (o *snapshotter) remoteMountWithExtraOptions(ctx context.Context, s storage
 			Options: overlayOptions,
 		},
 	}, nil
+}
+
+// Consts and data structures for Kata Virtual Volume
+const (
+	minBlockSize = 1 << 9
+	maxBlockSize = 1 << 19
+)
+
+const (
+	KataVirtualVolumeOptionName          = "io.katacontainers.volume"
+	KataVirtualVolumeDirectBlockType     = "direct_block"
+	KataVirtualVolumeImageRawBlockType   = "image_raw_block"
+	KataVirtualVolumeLayerRawBlockType   = "layer_raw_block"
+	KataVirtualVolumeImageNydusBlockType = "image_nydus_block"
+	KataVirtualVolumeLayerNydusBlockType = "layer_nydus_block"
+	KataVirtualVolumeImageNydusFsType    = "image_nydus_fs"
+	KataVirtualVolumeLayerNydusFsType    = "layer_nydus_fs"
+	KataVirtualVolumeImageGuestPullType  = "image_guest_pull"
+)
+
+// DmVerityInfo contains configuration information for DmVerity device.
+type DmVerityInfo struct {
+	HashType  string `json:"hashtype"`
+	Hash      string `json:"hash"`
+	BlockNum  uint64 `json:"blocknum"`
+	Blocksize uint64 `json:"blocksize"`
+	Hashsize  uint64 `json:"hashsize"`
+	Offset    uint64 `json:"offset"`
+}
+
+func (d *DmVerityInfo) Validate() error {
+	err := d.validateHashType()
+	if err != nil {
+		return err
+	}
+
+	if d.BlockNum == 0 || d.BlockNum > uint64(^uint32(0)) {
+		return fmt.Errorf("Zero block count for DmVerity device %s", d.Hash)
+	}
+
+	if !validateBlockSize(d.Blocksize) || !validateBlockSize(d.Hashsize) {
+		return fmt.Errorf("Unsupported verity block size: data_block_size = %d, hash_block_size = %d", d.Blocksize, d.Hashsize)
+	}
+
+	if d.Offset%d.Hashsize != 0 || d.Offset < d.Blocksize*d.BlockNum {
+		return fmt.Errorf("Invalid hashvalue offset %d for DmVerity device %s", d.Offset, d.Hash)
+	}
+
+	return nil
+}
+
+func (d *DmVerityInfo) validateHashType() error {
+	switch strings.ToLower(d.HashType) {
+	case "sha256":
+		return d.validateHash(64, "sha256")
+	case "sha1":
+		return d.validateHash(40, "sha1")
+	default:
+		return fmt.Errorf("Unsupported hash algorithm %s for DmVerity device %s", d.HashType, d.Hash)
+	}
+}
+
+func (d *DmVerityInfo) validateHash(expectedLen int, hashType string) error {
+	_, err := hex.DecodeString(d.Hash)
+	if len(d.Hash) != expectedLen || err != nil {
+		return fmt.Errorf("Invalid hash value %s:%s for DmVerity device with %s", hashType, d.Hash, hashType)
+	}
+	return nil
+}
+
+func validateBlockSize(blockSize uint64) bool {
+	return minBlockSize <= blockSize && blockSize <= maxBlockSize
+}
+
+func ParseDmVerityInfo(option string) (*DmVerityInfo, error) {
+	no := &DmVerityInfo{}
+	if err := json.Unmarshal([]byte(option), no); err != nil {
+		return nil, errors.Wrapf(err, "DmVerityInfo json unmarshal err")
+	}
+	if err := no.Validate(); err != nil {
+		return nil, fmt.Errorf("DmVerityInfo is not correct, %+v; error = %+v", no, err)
+	}
+	return no, nil
+}
+
+// DirectAssignedVolume contains meta information for a directly assigned volume.
+type DirectAssignedVolume struct {
+	Metadata map[string]string `json:"metadata"`
+}
+
+func (d *DirectAssignedVolume) Validate() bool {
+	return d.Metadata != nil
+}
+
+// ImagePullVolume contains meta information for pulling an image inside the guest.
+type ImagePullVolume struct {
+	Metadata map[string]string `json:"metadata"`
+}
+
+func (i *ImagePullVolume) Validate() bool {
+	return i.Metadata != nil
+}
+
+// NydusImageVolume contains Nydus image volume information.
+type NydusImageVolume struct {
+	Config      string `json:"config"`
+	SnapshotDir string `json:"snapshot_dir"`
+}
+
+func (n *NydusImageVolume) Validate() bool {
+	return len(n.Config) > 0 || len(n.SnapshotDir) > 0
+}
+
+// KataVirtualVolume encapsulates information for extra mount options and direct volumes.
+type KataVirtualVolume struct {
+	VolumeType   string                `json:"volume_type"`
+	Source       string                `json:"source,omitempty"`
+	FSType       string                `json:"fs_type,omitempty"`
+	Options      []string              `json:"options,omitempty"`
+	DirectVolume *DirectAssignedVolume `json:"direct_volume,omitempty"`
+	ImagePull    *ImagePullVolume      `json:"image_pull,omitempty"`
+	NydusImage   *NydusImageVolume     `json:"nydus_image,omitempty"`
+	DmVerity     *DmVerityInfo         `json:"dm_verity,omitempty"`
+}
+
+func (k *KataVirtualVolume) Validate() bool {
+	switch k.VolumeType {
+	case KataVirtualVolumeDirectBlockType:
+		if k.Source != "" && k.DirectVolume != nil && k.DirectVolume.Validate() {
+			return true
+		}
+	case KataVirtualVolumeImageRawBlockType, KataVirtualVolumeLayerRawBlockType:
+		if k.Source != "" && (k.DmVerity == nil || k.DmVerity.Validate() == nil) {
+			return true
+		}
+	case KataVirtualVolumeImageNydusBlockType, KataVirtualVolumeLayerNydusBlockType, KataVirtualVolumeImageNydusFsType, KataVirtualVolumeLayerNydusFsType:
+		if k.Source != "" && k.NydusImage != nil && k.NydusImage.Validate() {
+			return true
+		}
+	case KataVirtualVolumeImageGuestPullType:
+		if k.ImagePull != nil && k.ImagePull.Validate() {
+			return true
+		}
+	}
+
+	return false
+}
+
+func ParseKataVirtualVolume(option []byte) (*KataVirtualVolume, error) {
+	no := &KataVirtualVolume{}
+	if err := json.Unmarshal(option, no); err != nil {
+		return nil, errors.Wrapf(err, "KataVirtualVolume json unmarshal err")
+	}
+	if !no.Validate() {
+		return nil, fmt.Errorf("KataVirtualVolume is not correct, %+v", no)
+	}
+
+	return no, nil
+}
+
+func ParseKataVirtualVolumeFromBase64(option string) (*KataVirtualVolume, error) {
+	opt, err := base64.StdEncoding.DecodeString(option)
+	if err != nil {
+		return nil, errors.Wrap(err, "KataVirtualVolume base64 decoding err")
+	}
+	return ParseKataVirtualVolume(opt)
+}
+
+func EncodeKataVirtualVolumeToBase64(volume KataVirtualVolume) (string, error) {
+	validKataVirtualVolumeJSON, err := json.Marshal(volume)
+	if err != nil {
+		return "", errors.Wrapf(err, "marshal KataVirtualVolume object")
+	}
+	option := base64.StdEncoding.EncodeToString(validKataVirtualVolumeJSON)
+	return option, nil
 }
