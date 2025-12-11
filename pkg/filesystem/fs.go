containerd/nydus-snapshotter

commit b5bceed6ff5bd1c4d941569a3161e24633b6ae4d

support restart snapshotter trigger hotUpgrade
Signed-off-by: Zephyr <zinsist777@gmail.com>

pkg/filesystem/fs.go
@@ -41,16 +41,16 @@ import (
 )
 
 type Filesystem struct {
-	fusedevSharedDaemon  *daemon.Daemon
-	fscacheSharedDaemon  *daemon.Daemon
-	enabledManagers      map[string]*manager.Manager
-	cacheMgr             *cache.Manager
-	referrerMgr          *referrer.Manager
-	stargzResolver       *stargz.Resolver
-	tarfsMgr             *tarfs.Manager
-	verifier             *signature.Verifier
-	nydusImageBinaryPath string
-	rootMountpoint       string
+	fusedevSharedDaemon *daemon.Daemon
+	fscacheSharedDaemon *daemon.Daemon
+	enabledManagers     map[string]*manager.Manager
+	cacheMgr            *cache.Manager
+	referrerMgr         *referrer.Manager
+	stargzResolver      *stargz.Resolver
+	tarfsMgr            *tarfs.Manager
+	verifier            *signature.Verifier
+	nydusdBinaryPath    string
+	rootMountpoint      string
 }
 
 // NewFileSystem initialize Filesystem instance
@@ -121,36 +121,75 @@ func NewFileSystem(ctx context.Context, opt ...NewFSOpt) (*Filesystem, error) {
 	}
 
 	// Try to bring all persisted and stopped nydusd up and remount Rafs
-	eg, _ := errgroup.WithContext(context.Background())
+	egRecover, _ := errgroup.WithContext(context.Background())
 	for _, d := range recoveringDaemons {
 		d := d
-		eg.Go(func() error {
+		egRecover.Go(func() error {
 			d.ClearVestige()
 			fsManager, err := fs.getManager(d.States.FsDriver)
 			if err != nil {
-				return errors.Wrapf(err, "get filesystem manager for daemon %s", d.States.ID)
+				log.L.Warnf("Failed to get filesystem manager for daemon %s, skipping recovery: %v", d.States.ID, err)
+				return nil
 			}
 			if err := fsManager.StartDaemon(d); err != nil {
-				return errors.Wrapf(err, "start daemon %s", d.ID())
+				log.L.Warnf("Failed to start daemon %s during recovery, skipping: %v", d.ID(), err)
+				return nil
 			}
 			if err := d.WaitUntilState(types.DaemonStateRunning); err != nil {
-				return errors.Wrapf(err, "wait for daemon %s", d.ID())
+				log.L.Warnf("Failed to wait for daemon %s to become running, skipping: %v", d.ID(), err)
+				return nil
 			}
 			if err := d.RecoverRafsInstances(); err != nil {
-				return errors.Wrapf(err, "recover mounts for daemon %s", d.ID())
+				log.L.Warnf("Failed to recover mounts for daemon %s, skipping: %v", d.ID(), err)
+				return nil
 			}
 			fs.TryRetainSharedDaemon(d)
 			return nil
 		})
 	}
-	if err := eg.Wait(); err != nil {
+	if err := egRecover.Wait(); err != nil {
 		return nil, err
 	}
 
-	for _, d := range liveDaemons {
-		fs.TryRetainSharedDaemon(d)
+	newNydusImageBinaryCommit, err := daemon.GetDaemonGitCommit(fs.nydusdBinaryPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get git commit from nydusd binary at path: %s", fs.nydusdBinaryPath)
 	}
 
+	egLive, _ := errgroup.WithContext(context.Background())
+	for _, d := range liveDaemons {
+		d := d
+		egLive.Go(func() error {
+			if d.Supervisor == nil {
+				log.L.Warnf("Daemon %s is skipped for hot upgrade because recover policy is not set to 'failover'", d.ID())
+				return nil
+			}
+			daemonInfo, err := d.GetDaemonInfo()
+			if err != nil {
+				log.L.Warnf("Failed to get daemon info from daemon %s, skipping: %v", d.ID(), err)
+				return nil
+			}
+			if newNydusImageBinaryCommit != daemonInfo.DaemonVersion().GitCommit {
+				fsManager, err := fs.getManager(d.States.FsDriver)
+				if err != nil {
+					log.L.Warnf("Failed to get filesystem manager for daemon %s, skipping: %v", d.ID(), err)
+					return nil
+				}
+				newDaemon, upgradeErr := fsManager.DoDaemonUpgrade(d, fs.nydusdBinaryPath, fsManager)
+				if upgradeErr != nil {
+					log.L.Warnf("Daemon %s hot upgrade failed, skipping: %v", d.ID(), upgradeErr)
+					return nil
+				}
+				fs.TryRetainSharedDaemon(newDaemon)
+			} else {
+				fs.TryRetainSharedDaemon(d)
+			}
+			return nil
+		})
+	}
+	if err := egLive.Wait(); err != nil {
+		return nil, err
+	}
 	return &fs, nil
 }
 

.gitignore
@@ -6,3 +6,5 @@ tests/output/
 smoke.tests
 tools/optimizer-server/target
 vendor/
+.idea/
+.DS_Store

pkg/daemon/daemon.go
@@ -9,8 +9,11 @@ package daemon
 
 import (
 	"os"
+	"os/exec"
 	"path/filepath"
+	"regexp"
 	"sort"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"syscall"
@@ -649,4 +652,18 @@ func NewDaemon(opt ...NewDaemonOpt) (*Daemon, error) {
 	}
 
 	return d, nil
+}
+
+func GetDaemonGitCommit(nydusdPath string) (string, error) {
+	cmd := exec.Command(nydusdPath, "--version")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to run %s -V", nydusdPath)
+	}
+	re := regexp.MustCompile(`Git Commit:\s*(.+)`)
+	matches := re.FindStringSubmatch(string(output))
+	if len(matches) > 1 {
+		return strings.TrimSpace(matches[1]), nil
+	}
+	return "", errors.New("Git Commit not found in nydusd -V output")
 }

pkg/filesystem/config.go
@@ -19,9 +19,9 @@ import (
 
 type NewFSOpt func(d *Filesystem) error
 
-func WithNydusImageBinaryPath(p string) NewFSOpt {
+func WithNydusdBinaryPath(p string) NewFSOpt {
 	return func(fs *Filesystem) error {
-		fs.nydusImageBinaryPath = p
+		fs.nydusdBinaryPath = p
 		return nil
 	}
 }

pkg/filesystem/stargz_adaptor.go
@@ -138,7 +138,7 @@ func (fs *Filesystem) MergeStargzMetaLayer(ctx context.Context, s storage.Snapsh
 			"--bootstrap", tf.Name(),
 		}
 		options = append(options, bootstraps...)
-		cmd := exec.Command(fs.nydusImageBinaryPath, options...)
+		cmd := exec.Command(fs.nydusdBinaryPath, options...)
 		cmd.Stderr = os.Stderr
 		cmd.Stdout = os.Stdout
 		log.G(ctx).Infof("nydus image command %v", options)
@@ -238,7 +238,7 @@ func (fs *Filesystem) PrepareStargzMetaLayer(blob *stargz.Blob, storagePath stri
 		"--blob-meta", blobMetaPath,
 	}
 	options = append(options, filepath.Join(storagePath, stargz.TocFileName))
-	cmd := exec.Command(fs.nydusImageBinaryPath, options...)
+	cmd := exec.Command(fs.nydusdBinaryPath, options...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	log.L.Infof("nydus image command %v", options)

pkg/manager/daemon_event.go
@@ -8,6 +8,11 @@
 package manager
 
 import (
+	"fmt"
+	"path"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/containerd/log"
@@ -129,4 +134,110 @@ func (m *Manager) doDaemonRestart(d *daemon.Daemon) {
 			log.L.Warnf("Failed to mount rafs instance, %v", err)
 		}
 	}
+}
+
+// Provide minimal parameters since most of it can be recovered by nydusd states.
+// Create a new daemon in Manger to take over the service.
+func (m *Manager) DoDaemonUpgrade(d *daemon.Daemon, nydusdPath string, manager *Manager) (*daemon.Daemon, error) {
+	supervisor := d.Supervisor
+
+	newDaemon := &daemon.Daemon{
+		States:     d.States,
+		Supervisor: supervisor,
+	}
+	newDaemon.CloneRafsInstances(d)
+
+	s := path.Base(d.GetAPISock())
+	next, err := buildNextAPISocket(s)
+	if err != nil {
+		return nil, err
+	}
+
+	upgradingSocket := path.Join(path.Dir(d.GetAPISock()), next)
+	newDaemon.States.APISocket = upgradingSocket
+
+	cmd, err := manager.BuildDaemonCommand(newDaemon, nydusdPath, true)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := supervisor.SendStatesTimeout(time.Second * 10); err != nil {
+		return nil, errors.Wrap(err, "Send states")
+	}
+
+	if err := cmd.Start(); err != nil {
+		return nil, errors.Wrap(err, "start process")
+	}
+
+	newDaemon.States.ProcessID = cmd.Process.Pid
+
+	if err := newDaemon.WaitUntilState(types.DaemonStateInit); err != nil {
+		return nil, errors.Wrap(err, "wait until init state")
+	}
+
+	if err := newDaemon.TakeOver(); err != nil {
+		return nil, errors.Wrap(err, "take over resources")
+	}
+
+	if err := newDaemon.WaitUntilState(types.DaemonStateReady); err != nil {
+		return nil, errors.Wrap(err, "wait unit ready state")
+	}
+
+	if err := manager.UnsubscribeDaemonEvent(d); err != nil {
+		return nil, errors.Wrap(err, "unsubscribe daemon event")
+	}
+
+	// Let the older daemon exit without umount
+	if err := d.Exit(); err != nil {
+		return nil, errors.Wrap(err, "old daemon exits")
+	}
+
+	if err := newDaemon.Start(); err != nil {
+		return nil, errors.Wrap(err, "start file system service")
+	}
+
+	if err := manager.SubscribeDaemonEvent(newDaemon); err != nil {
+		return nil, errors.Wrap(err, "subscribe new daemon event")
+	}
+
+	if err := newDaemon.WaitUntilState(types.DaemonStateRunning); err != nil {
+		return nil, errors.Wrapf(err, "wait for daemon %s", d.ID())
+	}
+	if err := newDaemon.RecoverRafsInstances(); err != nil {
+		return nil, errors.Wrapf(err, "recover mounts for daemon %s", d.ID())
+	}
+
+	log.L.Infof("Started service of upgraded daemon on socket %s", newDaemon.GetAPISock())
+
+	if err := manager.UpdateDaemon(newDaemon); err != nil {
+		return nil, err
+	}
+
+	log.L.Infof("Upgraded daemon success on socket %s", newDaemon.GetAPISock())
+	return newDaemon, err
+}
+
+// Name next api socket path based on currently api socket path listened on.
+// The principle is to add a suffix number to api[0-9]+.sock
+func buildNextAPISocket(cur string) (string, error) {
+	n := strings.Split(cur, ".")
+	if len(n) != 2 {
+		return "", errors.Errorf("invalid api socket path format: %s", cur)
+	}
+	r := regexp.MustCompile(`[0-9]+`)
+	m := r.Find([]byte(n[0]))
+	var num int
+	if m == nil {
+		num = 1
+	} else {
+		var err error
+		num, err = strconv.Atoi(string(m))
+		if err != nil {
+			return "", err
+		}
+		num++
+	}
+
+	nextSocket := fmt.Sprintf("api%d.sock", num)
+	return nextSocket, nil
 }

snapshot/snapshot.go
@@ -202,7 +202,7 @@ func NewSnapshotter(ctx context.Context, cfg *config.SnapshotterConfig) (snapsho
 
 	opts := []filesystem.NewFSOpt{
 		filesystem.WithManagers(fsManagers),
-		filesystem.WithNydusImageBinaryPath(cfg.DaemonConfig.NydusdPath),
+		filesystem.WithNydusdBinaryPath(cfg.DaemonConfig.NydusdPath),
 		filesystem.WithVerifier(verifier),
 		filesystem.WithRootMountpoint(config.GetRootMountpoint()),
 		filesystem.WithEnableStargz(cfg.Experimental.EnableStargz),

commit 9b62041f0d971de403e4e3a2c5a56e6b334d58d9

filesystem: adjust live daemon binary check
Commit b5bceed6ff introduced a regression where nydusd binary is
required even for proxy mode (used by Kata Containers for guest
pulling) where no nydusd daemon runs on the host.

The hot upgrade logic now unconditionally calls GetDaemonGitCommit()
which fails when nydusd binary doesn't exist. This breaks proxy mode
deployments where only the snapshotter runs on the host.

Fix by only checking for the nydusd binary when nydusd is already
running.

Fixes regression from: b5bceed6ff5bd1c4d941569a3161e24633b6ae4d

Signed-off-by: Fabiano Fidêncio <ffidencio@nvidia.com>
Signed-off-by: Zephyr <zinsist777@gmail.com>

pkg/filesystem/fs.go
@@ -157,44 +157,47 @@ func NewFileSystem(ctx context.Context, opt ...NewFSOpt) (*Filesystem, error) {
 		return nil, err
 	}
 
-	newNydusImageBinaryCommit, err := daemon.GetDaemonGitCommit(fs.nydusdBinaryPath)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to get git commit from nydusd binary at path: %s", fs.nydusdBinaryPath)
-	}
+	// Only check nydusd binary commit when there are live daemons that may need hot upgrade.
+	if len(liveDaemons) > 0 {
+		newNydusImageBinaryCommit, err := daemon.GetDaemonGitCommit(fs.nydusdBinaryPath)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to get git commit from nydusd binary at path: %s", fs.nydusdBinaryPath)
+		}
 
-	egLive, _ := errgroup.WithContext(context.Background())
-	for _, d := range liveDaemons {
-		d := d
-		egLive.Go(func() error {
-			if d.Supervisor == nil {
-				log.L.Warnf("Daemon %s is skipped for hot upgrade because recover policy is not set to 'failover'", d.ID())
-				return nil
-			}
-			daemonInfo, err := d.GetDaemonInfo()
-			if err != nil {
-				log.L.Warnf("Failed to get daemon info from daemon %s, skipping: %v", d.ID(), err)
-				return nil
-			}
-			if newNydusImageBinaryCommit != daemonInfo.DaemonVersion().GitCommit {
-				fsManager, err := fs.getManager(d.States.FsDriver)
+		egLive, _ := errgroup.WithContext(context.Background())
+		for _, d := range liveDaemons {
+			d := d
+			egLive.Go(func() error {
+				if d.Supervisor == nil {
+					log.L.Warnf("Daemon %s is skipped for hot upgrade because recover policy is not set to 'failover'", d.ID())
+					return nil
+				}
+				daemonInfo, err := d.GetDaemonInfo()
 				if err != nil {
-					log.L.Warnf("Failed to get filesystem manager for daemon %s, skipping: %v", d.ID(), err)
+					log.L.Warnf("Failed to get daemon info from daemon %s, skipping: %v", d.ID(), err)
 					return nil
 				}
-				newDaemon, upgradeErr := fsManager.DoDaemonUpgrade(d, fs.nydusdBinaryPath, fsManager)
-				if upgradeErr != nil {
-					log.L.Warnf("Daemon %s hot upgrade failed, skipping: %v", d.ID(), upgradeErr)
-					return nil
+				if newNydusImageBinaryCommit != daemonInfo.DaemonVersion().GitCommit {
+					fsManager, err := fs.getManager(d.States.FsDriver)
+					if err != nil {
+						log.L.Warnf("Failed to get filesystem manager for daemon %s, skipping: %v", d.ID(), err)
+						return nil
+					}
+					newDaemon, upgradeErr := fsManager.DoDaemonUpgrade(d, fs.nydusdBinaryPath, fsManager)
+					if upgradeErr != nil {
+						log.L.Warnf("Daemon %s hot upgrade failed, skipping: %v", d.ID(), upgradeErr)
+						return nil
+					}
+					fs.TryRetainSharedDaemon(newDaemon)
+				} else {
+					fs.TryRetainSharedDaemon(d)
 				}
-				fs.TryRetainSharedDaemon(newDaemon)
-			} else {
-				fs.TryRetainSharedDaemon(d)
-			}
-			return nil
-		})
-	}
-	if err := egLive.Wait(); err != nil {
-		return nil, err
+				return nil
+			})
+		}
+		if err := egLive.Wait(); err != nil {
+			return nil, err
+		}
 	}
 	return &fs, nil
 }